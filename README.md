7b11db50f4fd352fe7644748716ca47e2d846c03
S Abhinav <sabhinav@S-Abhinav.local>
2021-04-03 14:29:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# SOZO
+## __The Advanced SOS Application
+
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)
+
+SOZO is a advanced SOS mobile application that simplifies the process of calling for help. With its efficient flow the app aids in saving more lives. 
+No matter where you are, when in need, SOZO can also call for help for your near & dear ones.  
+
+- Life - Saving procedures digitized .
+- The Auothorities and locals are the first one to save lifes .
+
+
+> A BBC investigation revealed that
+> seriously ill and injured patients wait for more than an hour  
+> to access an Ambulance and for any medical assistance. 
+
+## Features
+
+- Import a HTML file and watch it magically convert to Markdown
+- Drag and drop images (requires your Dropbox account be linked)
+- Import and save files from GitHub, Dropbox, Google Drive and One Drive
+- Drag and drop markdown and HTML files into Dillinger
+- Export documents as Markdown, HTML and PDF
+ 
+
+
+SOZO mainly focuses on 
+## Tech
+
+SOZO uses Googgle Tecnologies :
+
+- [Flutter](https://flutter.dev/docs) - Google’s UI toolkit for building beautiful, natively compiled applications for mobile, web, and desktop from a single codebase.
+- [Firebase](https://firebase.google.com/docs) -  Google's mobile platform that helps you quickly develop high-quality apps
+- [Android](https://codelabs.developers.google.com/?authuser=1) - Mobile Operating System 
+
+
+## Development
+
+Our inspiration for selecting these specific goals was to make people feel in control of their situation and that they are not alone. 
+Be it any kind of help that is needed by the person, a medical emergency, an accident, a vehicle breakdown, a disaster, or any crime incident, help is always on the way. 
+We felt like so many crimes and emergencies can be resolved easily if only the person could ask for help and with this application, they finally have a voice. 
+You can even help a person near you with the help of this application which alerts all the users and authorities near the person in need for the kind of help needed and volunteers can accept the request for help hence achieving partnership for the goals.
+
+##Challenge 
+
+Our vision is to connect the people stuck in any sort of difficult situations with those who are ready to extend their help. The app serves to bridge and connect the needy with a volunteer in a most efficient way and on a real time basis.
+
+
---
+++ lib/loginpage.dart
@@ -0,0 +1,260 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class loginpage extends StatefulWidget {
+  @override
+  _MyState createState() => _MyState();
+}
+
+class _MyState extends State<loginpage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passController = TextEditingController();
+  String _UserName = '';
+  String _Password ='';
+
+String validator(String svalue){
+   if (svalue.trim().isEmpty) {
+     return "empty";
+   }
+   return svalue;
+ }
+  Future<void> _createUserWithMail() async{
+    try{
+      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: _UserName, password: _Password);
+    } on FirebaseAuthException catch(e){
+      print("error : $e");
+    }
+    catch(e){
+      print("error : $e");
+    }
+  }
+  Future<void> _loginUserWithMail() async{
+    try{
+      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(email: _UserName, password: _Password);
+    } on FirebaseAuthException catch(e){
+      print("error : $e");
+    }
+    catch(e){
+      print("error : $e");
+    }
+  }
+
+  Future<void> _createUser() async{
+    try{
+      UserCredential userCredential = await FirebaseAuth.instance.signInAnonymously();
+    } on FirebaseAuthException catch(e){
+      print("error : $e");
+    }
+    catch(e){
+      print("error : $e");
+    }
+  }
+
+/* Future<void> _createActUser() async{
+
+    FirebaseAuth auth = FirebaseAuth.instance;
+    await auth.verifyPhoneNumber(
+      phoneNumber: '+44 7123 123 456',
+        verificationCompleted: (PhoneAuthCredential credential) async {
+          // ANDROID ONLY!
+
+          // Sign the user in (or link) with the auto-generated credential
+          await auth.signInWithCredential(credential);
+        },
+        verificationFailed: (FirebaseAuthException e) {
+          if (e.code == 'invalid-phone-number') {
+            print('The provided phone number is not valid.');
+          };
+          codeSent: (String verificationId, int resendToken) async {
+            // Update the UI - wait for the user to enter the SMS code
+            String smsCode = 'xxxx';
+
+            // Create a PhoneAuthCredential with the code
+            PhoneAuthCredential phoneAuthCredential = PhoneAuthProvider.credential(verificationId: verificationId, smsCode: smsCode);
+
+            // Sign the user in (or link) with the credential
+            await auth.signInWithCredential(phoneAuthCredential);
+          },
+          timeout: const Duration(seconds: 60),
+    codeAutoRetrievalTimeout: (String verificationId) {
+    // Auto-resolution timed out...
+    },
+    );
+      //codeAutoRetrievalTimeout: (String verificationId) {},
+
+  }}*/
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      resizeToAvoidBottomInset: true,
+        body: SafeArea(
+      child:
+
+
+
+
+
+                 Center(
+                   child: SingleChildScrollView(
+                      child: Container(
+
+                        child: Column(
+
+                            children: [
+
+
+
+                              SizedBox(
+                                child: Text("SOZO",
+                                  style: TextStyle(fontWeight: FontWeight.bold,color: Colors.redAccent, fontSize: 65,),
+                                ),
+                              ),
+                              Container(
+                                height: 250.0,
+                                width: 250.0,
+                                padding: EdgeInsets.only(top: 5),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(200),
+                                ),
+                                child: Center(
+                                  child: Image(
+                                      image:AssetImage ('assets/sos.png'),
+                                    height: 240,
+                                    width : 240,
+                                  ),
+
+                                ),
+                              ),
+                            Container(
+                                margin: EdgeInsets.all(20),
+                                child: TextField(
+                                  controller: nameController,
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: 'EmailID',
+                                  ),
+                                  onChanged: (text) {
+                                    setState(() {
+                                      _UserName = validator(text);
+
+                                    });
+                                  },
+                                  onSubmitted: (text){
+                                    _UserName = text;
+                            }
+                                )),
+                              Container(
+                                  margin: EdgeInsets.fromLTRB(20,0,20,20),
+                                  child: TextField(
+                                    controller: passController,
+                                    obscureText: true,
+                                    decoration: InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      labelText: 'Password',
+                                    ),
+
+
+
+
+
+                                    onChanged: (text) {
+                                      setState(() {
+                                        _Password = validator(text);
+
+
+                                      });
+                                    },
+                                    onSubmitted: (text){
+                                      _Password = text;
+                                    },
+                                  )),
+
+                            Center(
+                                child:
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        Container(
+                                          height: 40,
+                                          width: 150,
+                                          decoration: BoxDecoration(
+                                              color: Colors.redAccent, borderRadius: BorderRadius.circular(20)),
+                                          child: FlatButton(
+                                            onPressed:(){
+                                              _loginUserWithMail();
+                                            },
+
+                                            child: Text(
+                                              'Login',
+                                              style: TextStyle(color: Colors.white, fontSize: 15),
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          width: 10,
+                                        ),
+                                        Container(
+                                          height: 40,
+                                          width: 150,
+                                          decoration: BoxDecoration(
+                                              color: Colors.redAccent, borderRadius: BorderRadius.circular(20)),
+                                          child: FlatButton(
+                                            onPressed:(){
+                                              Navigator.pushNamed(context, '/signUpPage');
+                                            },
+
+                                            child: Text(
+                                              'Sign Up',
+                                              style: TextStyle(color: Colors.white, fontSize: 15),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+
+
+
+                                ),
+                              Container(
+                                height: 20,
+                                width: 20,
+                                child: Center(child: Text("or")),
+                              ),
+                              Center(
+                                child:
+                                Container(
+                                  height: 40,
+                                  width: 200,
+                                  decoration: BoxDecoration(
+                                      color: Colors.redAccent, borderRadius: BorderRadius.circular(20)),
+                                  child: FlatButton(
+                                    onPressed: _createUser,
+
+                                    child: Text(
+                                      'Login Anonymously',
+                                      style: TextStyle(color: Colors.white, fontSize: 15),
+                                    ),
+                                  ),
+                                ),
+
+
+
+                              ),
+
+                          ],
+                ),
+                      ),
+                    ),
+                 ),
+                  ),
+
+
+
+
+
+
+    );
+  }
+}
